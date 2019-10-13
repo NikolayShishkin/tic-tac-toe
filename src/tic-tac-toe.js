104b598c616f5e8d469633acd040649a37ac11c4
NikolayShishkin <r58korin@gmail.com>
2019-10-13 15:31:56 +0300

    feat: task done

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,81 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.gameField = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+    this.currentPlayerSymbol = 'x';
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayerSymbol;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (!this.gameField[rowIndex][columnIndex]) {
+      this.gameField[rowIndex][columnIndex] = this.currentPlayerSymbol;
+      if (this.currentPlayerSymbol === 'x') {
+        this.currentPlayerSymbol = 'o';
+      } else {
+        this.currentPlayerSymbol = 'x';
+      }
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return !!this.getWinner() || this.isDraw();
+  }
 
+  getWinner() {
+    // check rows
+    if(this.gameField[0][0] === this.gameField[0][1] && this.gameField[0][1]=== this.gameField[0][2]){
+      return this.gameField[0][0];
     }
-
-    nextTurn(rowIndex, columnIndex) {
-
+    if(this.gameField[1][0] === this.gameField[1][1] && this.gameField[1][1] === this.gameField[1][2]){
+      return this.gameField[1][0];
     }
-
-    isFinished() {
-
+    if(this.gameField[2][0] === this.gameField[2][1] && this.gameField[2][1] === this.gameField[2][2]){
+      return this.gameField[2][0];
     }
-
-    getWinner() {
-
+    //check columns
+    if(this.gameField[0][0] === this.gameField[1][0] && this.gameField[1][0] === this.gameField[2][0]){
+      return this.gameField[0][0];
     }
-
-    noMoreTurns() {
-
+    if(this.gameField[0][1] === this.gameField[1][1] && this.gameField[1][1] === this.gameField[2][1]){
+      return this.gameField[0][1];
     }
-
-    isDraw() {
-
+    if(this.gameField[0][2] === this.gameField[1][2] && this.gameField[1][2]=== this.gameField[2][2]){
+      return this.gameField[0][2];
     }
-
-    getFieldValue(rowIndex, colIndex) {
-
+    //check diagonals
+    if(this.gameField[0][0] === this.gameField[1][1] && this.gameField[1][1] === this.gameField[2][2]){
+      return this.gameField[0][0];
     }
+    if(this.gameField[0][2] === this.gameField[1][1] && this.gameField[1][1] === this.gameField[2][0]){
+      return this.gameField[0][2];
+    }
+    //if no winner, return null
+    return null;
+  }
+
+  noMoreTurns() {
+    if(!this.gameField[0][0] || !this.gameField[0][1] || !this.gameField[0][2] || !this.gameField[1][0]
+      || !this.gameField[1][1] || !this.gameField[1][2] || !this.gameField[2][0] || !this.gameField[2][1] 
+      || !this.gameField[2][2]){
+        return false;
+      } else{
+        return true;
+      }
+  }
+
+  isDraw() {
+    return this.noMoreTurns() && !this.getWinner();
+  }
+
+  getFieldValue(rowIndex, colIndex) {
+      return this.gameField[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
